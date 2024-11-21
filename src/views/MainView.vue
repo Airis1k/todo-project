c2ccd9b6238889ce1c92fbce7bc653cde4001c5e
Airis1k <airingasstar@gmail.com>
2024-11-21 13:46:23 +0200

    setup initial project structure

---
+++ src/views/MainView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+   <div>Tasks</div>
+</template>
+
+<style scoped>
+div {
+   background-color: #2b1887;
+   height: 100vh;
+}
+</style>
---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+   <div>404</div>
+   <div>Page not found</div>
+</template>
